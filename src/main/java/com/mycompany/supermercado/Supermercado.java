97743e9690d1869588e82dfa0fecee51e958cd0a
Eva <albaevansu@gmail.com>
2023-07-09 19:26:33 +0200

    conexion supermercado

--- src/main/java/com/mycompany/supermercado/Supermercado.java
+++ src/main/java/com/mycompany/supermercado/Supermercado.java
@@ -1,16 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.supermercado;
 
-/**
- *
- * @author anderjuarroseguren
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Supermercado {
 
+    protected static Connection conectar = null;
+    private final static String url = "jdbc:mysql://localhost/supermercado";
+    private final static String usuario = "root";
+    private final static String password = "admin";
+
     public static void main(String[] args) {
+
+        try {
+
+            conectar = DriverManager.getConnection(url, usuario, password);
+            System.out.println("Conexión Exitosa");
+        } catch (SQLException ex) {
+            System.out.println("Error al abrir Conexión: " + ex.getMessage());
+        }
+
         System.out.println("Hello World!");
     }
 }
